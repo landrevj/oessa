e35c8a9bc11f4759df3e7068d944fd3c90cabd3d
Joseph Landreville <landrevillejoseph@gmail.com>
2024-11-12 19:11:46 -0800

    disable autoimports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  imports: {
+    autoImport: false,
+  },
+  components: {
+    dirs: [],
+  },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
